f879c08192065b9e96a00f6e556325a7db7f8c2e
Teepu Khan <35717228+Teepu-Khan@users.noreply.github.com>
2018-01-29 13:32:48 -0800

    Create ch1Exercise.cpp

---
+++ Chapter 1 - Introduction/ch1Exercise.cpp
@@ -0,0 +1,20 @@
+// Big C++ 2nd Edition: P1.5
+//
+// This program simply demonstrates a basic understanding of the cout function.
+// It merely displays my name in a box.
+// 
+// Programmer: Teepu Khan
+// Date: 9/1/2017
+// Class: ch1Exercise.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "---------\n";
+	cout << "| Teepu |\n";
+	cout << "---------\n";
+
+    return 0;
+}
